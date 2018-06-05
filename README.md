75d14a5b29ae9049f24e010974bca3fe64243a14
Brendan MacDonald <bmacdonald@scottlogic.co.uk>
2018-06-05 14:02:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Run the following to install the necessary apps:
+```
+npm install 
+```
+
+Start Json Server with (optional - test will still pass without this since the server response is intercepted by nock):
+```
+npm run mock
+```
+
+Run the tests with: 
+```
+npm run test
+```
---
+++ __tests__/test1.js
@@ -0,0 +1,30 @@
+const frisby = require('frisby');
+var nock = require('nock');
+const config = require('./config.json');
+const Joi = frisby.Joi;
+
+// Nock will supply a default reply for all requests to http://localhost:3000/customers/1.
+// Nock will override what is actually on ../customers/1.
+// This endpoint doesn't need to be running for the test to pass.
+nock(config.baseURL)
+  .get(config.customerPath + '/1')
+  .reply(200, {
+    id: '123ABC',
+    first_name: 'XXXXX',
+    last_name: 'YYYYY',
+    phone: '111-111-11111111'
+  });
+
+it('GET against a mock endpoint AND intercept response with NOCK', function () {
+  return frisby
+    .get(config.baseURL + config.customerPath + '/1')
+    .then(function (res) {
+      expect(res.json.id).toBe('123ABC');
+      expect(res.json.first_name).toBe('XXXXX');
+      expect(res.json.last_name).toBe('YYYYY');
+      expect(res.json.phone).toBe('111-111-11111111');
+    })
+});
+
+
+
