57c12aea5b51f7fa6f8d15c633decabedcbf4895
Brendan MacDonald <bmacdonald@scottlogic.co.uk>
2018-06-07 09:26:52 +0100

    Added new test

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -2,11 +2,10 @@ const frisby = require('frisby');
 var nock = require('nock');
 const config = require('./config.json');
 
-// Nock will supply a default reply for all requests to http://localhost:3000/customers/1.
-// Nock will override what is actually on ../customers/1.
-// This endpoint doesn't need to be running for the test to pass.
+// Nock will supply a default reply for all requests to http://some-end-point:3000/users/1.
+// Nock will override what is actually on ../users/1 (if that endpoint actually exists)
 nock(config.baseURL)
-  .get(config.customerPath + '/1')
+  .get(config.path + '/1')
   .reply(200, {
     id: '123ABC',
     first_name: 'XXXXX',
@@ -16,7 +15,7 @@ nock(config.baseURL)
 
 it('GET against a mock endpoint AND intercept response with NOCK', function () {
   return frisby
-    .get(config.baseURL + config.customerPath + '/1')
+    .get(config.baseURL + config.path + '/1')
     .then(function (res) {
       expect(res.json.id).toBe('123ABC');
       expect(res.json.first_name).toBe('XXXXX');
@@ -25,5 +24,24 @@ it('GET against a mock endpoint AND intercept response with NOCK', function () {
     })
 });
 
+it('GET against a mock endpoint AND intercept response with NOCK', function () {
 
+  // Nock configured within a test.
+  nock(config.baseURL)
+    .get(config.path + '/1')
+    .reply(200, {
+      id: '456ABC',
+      first_name: 'AAA',
+      last_name: 'BBB',
+      phone: '111-111-22222222'
+    });
 
+  return frisby
+    .get(config.baseURL + config.path + '/1')
+    .then(function (res) {
+      expect(res.json.id).toBe('456ABC');
+      expect(res.json.first_name).toBe('AAA');
+      expect(res.json.last_name).toBe('BBB');
+      expect(res.json.phone).toBe('111-111-22222222');
+    })
+})
